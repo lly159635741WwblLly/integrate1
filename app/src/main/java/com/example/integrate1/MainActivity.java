de62278be451cfbffba59be4497510caf34324d7
jyc <1281940847@qq.com>
2022-08-24 12:03:34 +0800

    修补完成定位功能，应注意室内定位信号较差

--- app/src/main/java/com/example/integrate1/MainActivity.java
+++ app/src/main/java/com/example/integrate1/MainActivity.java
@@ -1,42 +1,113 @@
 package com.example.integrate1;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.UiSettings;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MyLocationStyle;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AMapLocationListener, LocationSource {
+
+    private MapView mapView;
     private AMap aMap;
-    MapView mMapView = null;
+    private OnLocationChangedListener mListener = null;//定位监听器
+    private boolean isFirstLoc = true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1_main);
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        //初始化控件
-        initUI();
-        //获取地图控件引用
-        mMapView = (MapView)findViewById(R.id.map);
+
+
+        mapView = findViewById(R.id.map);//找到地图控件
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
-        mMapView.onCreate(savedInstanceState);
+        mapView.onCreate(savedInstanceState);
+        aMap = mapView.getMap();//初始化地图控制器对象
 
-        //初始化地图控制器对象
-        if (aMap == null) {
-            aMap = mMapView.getMap();
+
+        //设置显示定位按钮 并且可以点击
+        UiSettings settings = aMap.getUiSettings();
+        //设置定位监听
+        aMap.setLocationSource(this);
+        //是否显示定位按钮
+        settings.setMyLocationButtonEnabled(true);
+        //是否可触发定位并显示定位层
+        aMap.setMyLocationEnabled(true);
+
+
+        //设置定位点的图标  默认为蓝色小点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        aMap.setMyLocationStyle(myLocationStyle);
+
+        //开始定位
+        try {
+            initLoc();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-    private void initUI() {
-        findViewById(R.id.button_my_pro).setOnClickListener(this);
+        //初始化
+        init();
 
     }
+
+    //定位
+    private void initLoc() throws Exception {
+
+        //初始化定位
+        //定位发起端
+        AMapLocationClient mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+
+
+        //初始化参数定位
+        //定位参数
+        AMapLocationClientOption mLocationClientOption = new AMapLocationClientOption();
+        //设置定位模式
+        mLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息，默认返回
+        mLocationClientOption.setNeedAddress(true);
+        //设置是否强制刷新WIFI，默认强制
+        mLocationClientOption.setWifiScan(true);
+        //设置定位间隔  ms
+        mLocationClientOption.setInterval(1000);
+
+        //给定客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationClientOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+
+
+
+    // 初始化AMap对象
+    void init() {
+
+            findViewById(R.id.button_my_pro).setOnClickListener(this);
+
+    }
+
 
     @Override
     public void onClick(View view) {
@@ -44,28 +115,103 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         intent.setClass(getApplicationContext(),ActivityMyProject.class);
         startActivity(intent);
     }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
-        mMapView.onDestroy();
+    //定位回调函数 必须重写
+    public void onLocationChanged(AMapLocation aMapLocation) {
+
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //定位成功 回调信息 设置相关信息
+                aMapLocation.getLocationType();//获取定位结果来源
+                aMapLocation.getLatitude();//获取纬度
+                aMapLocation.getLongitude();//获取经度
+                aMapLocation.getAccuracy();//获取精确信息
+
+                @SuppressLint("SimpleDateFormat") SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);
+
+                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                aMapLocation.getCountry();//国家信息
+                aMapLocation.getProvince();//省信息
+                aMapLocation.getCity();//城市信息
+                aMapLocation.getDistrict();//城区信息
+                aMapLocation.getStreet();//街道信息
+                aMapLocation.getStreetNum();//街道门牌号信息
+                aMapLocation.getCityCode();//城市编码
+                aMapLocation.getAdCode();//地区编码
+
+
+                if (isFirstLoc) {
+                    //缩放级别
+                    aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+                    //将地图移动到定位点
+                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(
+                            new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+                    //点击定位按钮 能够将地图的中心点移动到定位点
+                    mListener.onLocationChanged(aMapLocation);
+
+                    isFirstLoc = false;
+
+                }
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+
+                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
+            }
+        }
     }
+
+
+    //激活定位
     @Override
-    public void onResume() {
+    public void activate(OnLocationChangedListener listener) {
+        mListener = listener;
+
+    }
+
+    //停止定位
+    @Override
+    public void deactivate() {
+        mListener = null;
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
         super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
-        mMapView.onResume();
+        mapView.onResume();
     }
+
+    /**
+     * 方法必须重写
+     */
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
-        mMapView.onPause();
+        mapView.onPause();
     }
+
+    /**
+     * 方法必须重写
+     */
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
-        mMapView.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
 }
